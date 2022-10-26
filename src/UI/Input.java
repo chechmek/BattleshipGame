65f4a189b169988f8050f18c14982b976be674dc
onestep87 <philip88088@gmail.com>
2022-10-26 15:14:23 +0200

    Added getCoordinates and added simple UpdateMap(not final)

--- src/UI/Input.java
+++ src/UI/Input.java
@@ -2,15 +2,27 @@ package UI;
 
 import Abstract.IInput;
 import Abstract.Ship;
-import Models.Cell;
 import Models.Coordinate;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Input implements IInput
 {
     public Coordinate getCoordinates() {
-        return null;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a coordinate: ");
+
+        String coord= input.nextLine();
+        String[] parts = coord.split(",");
+
+        String x = parts[0].trim();
+        String y = parts[1].trim();
+        int X = Integer.parseInt(x);
+        int Y = Integer.parseInt(y);
+        Coordinate coordinate = new Coordinate(X,Y);
+        System.out.println("x: " + x + "\ny: " + y);
+        return coordinate;
     }
 
     public ArrayList<Ship> setShips() {
