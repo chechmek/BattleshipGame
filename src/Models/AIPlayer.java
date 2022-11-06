6e7545d007195a9f93b8bd63230ccdc4a08dce74
chechmek <romanklut89@gmail.com>
2022-11-06 12:17:44 +0100

    Omg its working

--- src/Models/AIPlayer.java
+++ src/Models/AIPlayer.java
@@ -1,11 +1,21 @@
 package Models;
 
 import Abstract.PlayerBase;
+import Abstract.Ship;
+import Ships.Carrier;
+
+import java.util.ArrayList;
 
 public class AIPlayer extends PlayerBase
 {
     public void setShips() {
 // Implement Ai
+        ArrayList<Ship> ships = new ArrayList<Ship>();
+        ships.add(new Carrier(
+                new Coordinate(2, 2),
+                new Coordinate(2,7)
+        ));
+        Map.setShips(ships);
     }
 
     public void Move() {
--- src/UI/Input.java
+++ src/UI/Input.java
@@ -52,18 +52,19 @@ public class Input implements IInput {
                 ships.add(setCarrierShip(input, localGrid));
                 showLocalGrid(localGrid);
             }
-            for (int i = 0; i < Battleship.Count; i++) {
-                ships.add(setBattleShip(input, localGrid));
-                showLocalGrid(localGrid);
-            }
-            for (int i = 0; i < Submarine.Count; i++) {
-                ships.add(setSubmarineShip(input, localGrid));
-                showLocalGrid(localGrid);
-            }
-            for (int i = 0; i < Patrol.Count; i++) {
-                ships.add(setPatrolShip(input, localGrid));
-                showLocalGrid(localGrid);
-            }
+            // commented for testing
+//            for (int i = 0; i < Battleship.Count; i++) {
+//                ships.add(setBattleShip(input, localGrid));
+//                showLocalGrid(localGrid);
+//            }
+//            for (int i = 0; i < Submarine.Count; i++) {
+//                ships.add(setSubmarineShip(input, localGrid));
+//                showLocalGrid(localGrid);
+//            }
+//            for (int i = 0; i < Patrol.Count; i++) {
+//                ships.add(setPatrolShip(input, localGrid));
+//                showLocalGrid(localGrid);
+//            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -71,28 +72,7 @@ public class Input implements IInput {
     }
 
     private void showLocalGrid(Cell[][] grid) {
-        System.out.print("\n===== YOUR SHIPS =====\n  A B C D E F G H I J");
-        for (int i = 0; i < grid.length; i++) {
-            System.out.println();
-            System.out.print(i + " ");
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j].State == State.Available) {
-                    System.out.print("I ");
-                } else if (grid[i][j].State == State.Unavailable) {
-                    System.out.print("X ");
-                } else if (grid[i][j].State == State.WithBattleship) {
-                    System.out.print("B ");
-                } else if (grid[i][j].State == State.WithCarrier) {
-                    System.out.print("C ");
-                } else if (grid[i][j].State == State.WithSubmarine) {
-                    System.out.print("S ");
-                } else if (grid[i][j].State == State.WithPatrol) {
-                    System.out.print("P ");
-                } else {
-                    System.out.print("Error");
-                }
-            }
-        }
+        UserInterface.drawGrid(grid, "YOUR SHIPS");
     }
 
     private final Dictionary<String, Integer> dict = new Hashtable<String, Integer>() {{
